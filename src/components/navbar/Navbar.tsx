42b271760e8564b92064f2117103e69e8f74659d
hellengleice <hellengleice.97@gmail.com>
2025-04-16 14:22:53 -0300

    Projeto Farmacia

---
+++ src/components/navbar/Navbar.tsx
@@ -0,0 +1,20 @@
+function Navbar() {
+    return (
+        <>
+            <div className='w-full flex justify-center py-4
+            			   bg-indigo-900 text-white'>
+            
+                <div className="container flex justify-between text-lg">
+                    Projeto Farmacia
+
+                    <div className='flex gap-4'>
+                        Lista Categorias
+                        Cadastrar Categoria
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Navbar
