04dc463bfcd40143204e2727af4935c8721e7b4c
hellengleice <hellengleice.97@gmail.com>
2025-04-17 08:40:03 -0300

    Projeto Farmacia

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,32 @@
-import Footer from './components/footer/Footer';
-import Navbar from './components/navbar/Navbar';
-import Home from './pages/home/Home';
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+
+import Navbar from './components/navbar/Navbar'
+import Home from './pages/home/Home'
+import './App.css'
+import ListaCategorias from './components/categoria/listacategorias/ListaCategorias'
+import DeletarCategoria from './components/categoria/deletarcategorias/DeletarCategoria'
+import CadastrarCategoria from './components/categoria/cadastrarcategoria/CadastrarCategoria'
+import Footer from './components/footer/Footer'
+
 
 function App() {
   return (
     <>
-      <Navbar />
-      <Home />
-      <Footer />
+        <BrowserRouter>
+          <Navbar />
+          <div className="min-h-[80vh]">
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/home" element={<Home />} />
+              <Route path="/categorias" element={<ListaCategorias />} />
+              <Route path="/deletarcategoria/:id" element={<DeletarCategoria />} />
+             <Route path="/cadastrarcategoria" element={<CadastrarCategoria />} />
+            </Routes>
+          </div>
+          <Footer />
+        </BrowserRouter>
     </>
-  );
+  )
 }
 
-export default App;
-
+export default App
--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,20 +1,26 @@
-function Navbar() {
-    return (
-        <>
-            <div className='w-full flex justify-center py-4
-            			   bg-indigo-900 text-white'>
-            
-                <div className="container flex justify-between text-lg">
-                    Projeto Farmacia
+import { Link } from "react-router-dom";
 
-                    <div className='flex gap-4'>
-                        Lista Categorias
-                        Cadastrar Categoria
-                    </div>
-                </div>
-            </div>
-        </>
-    )
+function Navbar() {
+  return (
+    <nav className="bg-blue-400 text-white shadow-md">
+      <div className="container mx-auto flex items-center justify-between px-6 py-4">
+        {/* Logo / Título */}
+        <Link to="/" className="text-2xl font-bold tracking-wide hover:text-indigo-300">
+          Projeto <span className="text-indigo-300">Farmacia</span>
+        </Link>
+
+        {/* Links de navegação */}
+        <div className="flex gap-6 text-lg font-medium">
+          <Link to="/categorias" className="hover:text-indigo-300 transition">
+            Categorias
+          </Link>
+          <Link to="/formularioCategoria" className="hover:text-indigo-300 transition">
+          <Link to="/cadastrarcategoria">Cadastrar Categoria</Link>
+          </Link>
+        </div>
+      </div>
+    </nav>
+  );
 }
 
-export default Navbar
+export default Navbar;
